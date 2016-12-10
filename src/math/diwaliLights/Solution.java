8c35d66d2f26185d5471d62d7e02f7204ec6c0a3
musta <belmokhtar.91@gmail.com>
2016-12-10 14:37:59 +0000

    fibonacci and diwali lights solved

---
+++ src/math/diwaliLights/Solution.java
@@ -0,0 +1,7 @@
+package math.diwaliLights;
+
+/**
+ * Created by DELL on 10/12/2016.
+ */
+public class Solution {
+}
---
+++ src/math/isFibo/Solution.java
@@ -0,0 +1,7 @@
+package math.isFibo;
+
+/**
+ * Created by DELL on 10/12/2016.
+ */
+public class Solution {
+}
