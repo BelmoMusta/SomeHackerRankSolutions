05f8dbb466a00b69315e079af53113352fa04691
musta <belmokhtar.91@gmail.com>
2017-10-14 18:20:23 +0100

    username regex validation

---
+++ src/main/java/javaProblems/strings/usernameregex/Solution.java
@@ -0,0 +1,35 @@
+package javaProblems.strings.usernameregex;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ * <a href="https://www.hackerrank.com/challenges/valid-username-checker/problem">Problem statement</a>
+ */
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression =  "[A-Za-z]([A-Za-z0-9_]){7,29}";
+}
+
+class Solution{
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+
+
+}
